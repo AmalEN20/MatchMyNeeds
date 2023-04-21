0e74742cd7673d60dafa5f12940c2e0e14a09681
AmalStudy <amalengstudy@gmail.com>
2023-04-21 02:44:58 -0700

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import MyRequests from './pages/MyRequests';
-import MyToDos from './pages/MyToDos';
-import Signup from './pages/Signup';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// FOR AUTH
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  // Sets up client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <>
       <Router>
-        <Header />
-        <div>
-            <Nav />
-            <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
-              <Route
-                path="/login"
-                element={<Login />}
-              />
-              <Route
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route
-                path="/myrequests"
-                element={<MyRequests />}
-              />
-              <Route
-                path="/mytodos"
-                element={<MyToDos />}
-              />
-              <Route
-                path="/logout"
-                element={<Logout />}
-              />
-              <Route
-                path="*"
-                element={<NoMatch />}
-              />
-            </Routes>
-            <Footer />
-        </div>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
       </Router>
-    </ApolloProvider>
+    </>
   );
 }
 
