4f6119b45d7a82dfdc239c6f2e6c378969ebc704
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-04-25 00:13:42 -0700

    edit App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,53 @@ import React from 'react';
 import Navbar from './NavBar/Navbar';
 import './App.css';
 import Home from './pages/Home';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Todo from './pages/Todo';
 import Request from './pages/Request';
 import SignUp from './pages/SignUp';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Router>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Todo} />
-          <Route path='/request' component={Request} />
+          <Route path='/me/todo' component={Todo} />
+          <Route path='/me/request' component={Request} />
           <Route path='/signup' component={SignUp} />
+          {/* <Route path='/requests/:requestId' component={SingleRequest}/> */}
         </Switch>
       </Router>
-    </>
+    </ApolloProvider>
   );
 }
 
