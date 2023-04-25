fc69fdf731d3a69b4fcb842beca21e606335ab05
AmalStudy <amalengstudy@gmail.com>
2023-04-24 21:21:28 -0700

    fixed footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,9 +7,6 @@ function Footer() {
   return (
     <div className='footer-container'>
       <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-        You can unsubscribe at any time.
-        </p>
 
         <div className='input-areas'>
 
