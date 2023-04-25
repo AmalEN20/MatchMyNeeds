fb0bd7f7c073f3cf4d15f339f73a9e16470825ce
Kristina Alabastro <kristinaa7@live.com>
2023-04-24 20:10:38 -0700

    added required files to request.js

--- client/src/components/pages/Request.js
+++ client/src/components/pages/Request.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import '../../App.css';
 
+import { ADD_REQUEST } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
 export default function Request() {
-  return <h1 className='products'>My Requests</h1>;
+  return (
+  <div> <h1 className='products'>My Requests</h1>
+
+  <h2>Add a request</h2>
+
+<form onSubmit={handleFormSubmit}></form>
+
+  </div>
+  );
 }
