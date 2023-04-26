9940fd84d3f84cb969595903dd940e8155017cd7
Kristina Alabastro <kristinaa7@live.com>
2023-04-26 11:38:29 -0700

    added form data to add request Item and request description

--- client/src/pages/Request.js
+++ client/src/pages/Request.js
@@ -1,16 +1,16 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import '../../src/App.css';
 
-import { ADD_REQUEST } from '../../utils/mutations';
-// import Auth from '../../utils/auth';
+import { ADD_REQUEST } from '../../src/utils/mutations';
+import Auth from '../../src/utils/auth';
 
-const RequestForm = ({ requestItem }) => {
+const RequestForm = ({ requestItem, requestDescription  }) => {
   const [request, setRequest] = useState('');
 
   const [addRequest, { error }] = useMutation(ADD_REQUEST);
-  
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -26,24 +26,48 @@ const RequestForm = ({ requestItem }) => {
   };
 
   return (
-  <div> 
-    <h1 className='request'> What are you looking for? </h1>
+    <div>
+      <h1 className='request'> What are you looking for? </h1>
 
-  <h2>Add a request</h2>
+      {Auth.loggedIn() ? (
+        <form
+          className="flex-row justify-center justify-space-between-md align-center"
+          onSubmit={handleFormSubmit}>
+          <div className="col-12 col-lg-9">
+            <input
+              placeholder="Type your request here."
+              value={ requestItem }
+              className="form-input w-100"
+              onChange={(event) => setRequest(event.target.value)}
+            />
+            <input
+              placeholder="Type the description here."
+              value={ requestDescription }
+              className="form-input w-100"
+              onChange={(event) => setRequest(event.target.value)}
+            />
+          </div>
 
-<form action="/" method="GET">
-  <div>
-    <label for="requestItem">Requested Item</label>
-    <input id="requestItem" name="requestItem" type="text"></input>
-  </div>
-  <div>
-    <label for="description">Description of Item</label>
-    <input id="description" name="description" type="text"></input>
-  </div>
-<button type="submit">Submit</button>
-</form>
-  </div>
+          <div className="col-12 col-lg-3">
+            <button className="btn btn-info btn-block py-3" type="submit">
+              Submit
+            </button>
+            <label> <input type="checkbox" /> Saved to My To-Dos</label>
+          </div>
+          {error && (
+            <div className="col-12 my-3 bg-danger text-white p-3">
+              {error.message}
+            </div>
+          )}
+        </form>
+      ) : (
+        <p>
+          You need to be logged in to add a request. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
   );
-}
+};
 
 export default Request;
