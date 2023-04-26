e49d6906157672dfc29086783c8d41586e061174
Kristina Alabastro <kristinaa7@live.com>
2023-04-26 11:15:00 -0700

    added request form using state and mutation

--- client/src/pages/Request.js
+++ client/src/pages/Request.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
 import '../../src/App.css';
 
 import { ADD_REQUEST } from '../../utils/mutations';
-import Auth from '../../utils/auth';
+// import Auth from '../../utils/auth';
+
+const RequestForm = ({ requestItem }) => {
+  const [request, setRequest] = useState('');
+
+  const [addSkill, { error }] = useMutation(ADD_REQUEST);
+  
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
 export default function Request() {
   return (
-  <div> <h1 className='products'>My Requests</h1>
+  <div> <h1 className='request'>My Requests</h1>
 
   <h2>Add a request</h2>
 
-<form onSubmit={handleFormSubmit}></form>
-
+<form action="/" method="GET">
+  <div>
+    <label for="requestItem">Requested Item</label>
+    <input id="requestItem" name="requestItem" type="text"></input>
+  </div>
+  <div>
+    <label for="description">Description of Item</label>
+    <input id="description" name="description" type="text"></input>
+  </div>
+<button type="submit">Submit</button>
+</form>
   </div>
   );
 }
