12360539e27abf4bad3fc65d7640546678f5f714
AmalStudy <amalengstudy@gmail.com>
2023-04-27 23:14:12 -0700

    1

--- client/src/components/AllRequests/index.js
+++ client/src/components/AllRequests/index.js
@@ -6,6 +6,23 @@ const RequestPosts = ({requests}) => {
 if (!requests.length) {
   return <h3>No Requests Yet</h3>
 }
+const styles = {
+  request: {
+    marginBottom: '20px',
+    border: '1px solid white',
+    padding: '10px'
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  title: {
+    marginBottom: '5px'
+  },
+  description: {
+    marginTop: '0',
+    color: 'gray'
+  }
+};
 
 return (
   <div style={{ 
@@ -15,31 +32,21 @@ return (
   }}>
     {requests &&
       requests.map((request) => (
-        <div key={request._id} style={{
-          marginBottom: '20px',
-          border: '1px solid black',
-          padding: '10px'
-        }}>
+        <div key={request._id} style={styles.request}>
           <Link
             to={`/requests/${request._id}`}
-            style={{
-              textDecoration: 'none'
-            }}
+            style={styles.link}
           >
             <div>
-              <h4 style={{
-                marginBottom: '5px'
-              }}>Requested Item: {request.requestItem}</h4>
-              <h5 style={{
-                marginTop: '0',
-                color: 'gray'
-              }}>Description: {request.requestDescription}</h5>
+              <h4 style={styles.title}>Requested Item: {request.requestItem}</h4>
+              <h5 style={styles.description}>Description: {request.requestDescription}</h5>
             </div>
           </Link>
         </div>
       ))}
   </div>
-);
+); 
+
 };
 
 export default RequestPosts;
