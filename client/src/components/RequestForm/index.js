7851070656abe9b3524c404299228ced3412219a
jordan <jlddukes@gmail.com>
2023-04-27 22:10:22 -0700

    added code to SingleRequest file

--- client/src/components/RequestForm/index.js
+++ client/src/components/RequestForm/index.js
@@ -1,32 +1,160 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// //Inport the 'useMutation()' hook from Apollo Client
+// import { useMutation } from '@apollo/client';
+// // import { QUERY_REQUESTS } from '../../utils/queries';
+
+// //Import the GraphQL mutation
+// import { ADD_REQUEST } from '../../utils/mutations';
+// // import Auth from '../../utils/auth';
+// import Auth from '../../utils/auth';
+
+// const RequestForm = ({ requestItem, requestDescription, location  }) => {
+//   const [request, setRequest] = useState('');
+
+//   //Invoke 'useMutation()' hook to return a promise-based function and data about the ADD_REQUEST
+//   const [addRequest, { error }] = useMutation(ADD_REQUEST);
+ 
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+  
+//     // Get the values entered by the user in the input fields
+//     const item = event.target.item.value;
+//     const description = event.target.description.value;
+//     const location = event.target.location.value;
+  
+//     // Since mutation function is async, wrap in try...catch to catch any network error
+//     try {
+//       // Execute mutation and pass in the values entered by the user as variables
+//       const { data } = await addRequest({
+//         variables: { item, description, location },
+//       });
+  
+//       setRequest('');
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+  
+//   // const handleFormSubmit = async (event) => {
+//   //   event.preventDefault();
+
+//   //   //Since mutation function is async, wrap in try...catch to catch any network error
+//   //   try {
+//   //     //Execute mutation and pass in defined parameter data as variables
+//   //     const data = await addRequest({
+//   //       variables: { requestItem, requestDescription, location },
+//   //     });
+
+//   //     setRequest('');
+//   //   } catch (err) {
+//   //     console.error(err);
+//   //   }
+//   // };
+
+//   return (
+//     <div>
+//       <h1> Add Your Requests Here </h1>
+
+//       {Auth.loggedIn() ? (
+//         <form onSubmit={handleFormSubmit}>
+//           <div>
+
+//           <label> <h3> Requested Item : </h3> </label>
+// <input
+//   placeholder="Type your request."
+//   type="text"
+//   name="item"
+//   onChange={(event) => setRequest(event.target.value)}
+// />
+
+// <label> <h3> Description of Item : </h3>  </label>
+// <input
+//   placeholder="Type the description."
+//   type="text"
+//   name="description"
+//   onChange={(event) => setRequest(event.target.value)}
+// />
+
+// <label> <h3> Location for Delivery : </h3>  </label>
+// <input
+//   placeholder="Type your city and state."
+//   type="text"
+//   name="location"
+//   onChange={(event) => setRequest(event.target.value)}
+// />
+
+//           {/* <label> <h3> Requested Item : </h3> </label>
+//             <input
+//               placeholder="Type your request."
+//               type = "text"
+//               value={request.requestItem }
+//               onChange={(event) => setRequest(event.target.value)}
+//             />
+//             <label> <h3> Description of Item : </h3>  </label>
+//             <input
+//               placeholder="Type the description."
+//               type = "text"
+//               value={ request.requestDescription }
+//               onChange={(event) => setRequest(event.target.value)}
+//             />
+//               <label> <h3> Location for Delivery : </h3>  </label>
+//             <input
+//               placeholder="Type your city and state."
+//               type = "text"
+//               value={ request.location }
+//               onChange={(event) => setRequest(event.target.value)}
+//             /> */}
+//           </div>
+
+//           <div>
+//             <button onClick={handleFormSubmit} className="btn btn-info btn-block py-3" type="submit">
+//               Submit your request
+//             </button>
+//           </div>
+//           {error && (
+//             <div className="col-12 my-3 bg-danger text-white p-3">
+//               {error.message}
+//             </div>
+//           )}
+//         </form>
+//       ) : (
+//         <p>
+//           You need to be logged in to add a request. Please{' '}
+//           <Link to="/signup">login.</Link>
+//         </p>
+//       )}
+//     </div>
+//   );
+//       }; 
+// export default RequestForm;
+
+
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-//Inport the 'useMutation()' hook from Apollo Client
 import { useMutation } from '@apollo/client';
-// import { QUERY_REQUESTS } from '../../utils/queries';
-
-//Import the GraphQL mutation
 import { ADD_REQUEST } from '../../utils/mutations';
-// import Auth from '../../utils/auth';
 import Auth from '../../utils/auth';
 
-const RequestForm = ({ requestItem, requestDescription, location  }) => {
-  const [request, setRequest] = useState('');
+const RequestForm = () => {
+  const [requestItem, setRequestItem] = useState('');
+  const [requestDescription, setRequestDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [response, setResponse] = useState(null);
 
-  //Invoke 'useMutation()' hook to return a promise-based function and data about the ADD_REQUEST
-  const [addRequest, { error }] = useMutation(ADD_REQUEST);
- 
+  const [addRequest, { error }] = useMutation(ADD_REQUEST, {
+    onCompleted: (data) => {
+      setResponse(data.addRequest);
+    },
+  });
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    //Since mutation function is async, wrap in try...catch to catch any network error
     try {
-      //Execute mutation and pass in defined parameter data as variables
-      const data = await addRequest({
+      await addRequest({
         variables: { requestItem, requestDescription, location },
       });
-
-      setRequest('');
     } catch (err) {
       console.error(err);
     }
@@ -34,36 +162,42 @@ const RequestForm = ({ requestItem, requestDescription, location  }) => {
 
   return (
     <div>
-      <h1> Add Your Requests Here </h1>
+      <h1>Add Your Requests Here</h1>
 
       {Auth.loggedIn() ? (
         <form onSubmit={handleFormSubmit}>
           <div>
-          <label> <h3> Requested Item : </h3> </label>
+            <label>
+              <h3>Requested Item:</h3>
+            </label>
             <input
               placeholder="Type your request."
-              type = "text"
-              value={request.requestItem }
-              onChange={(event) => setRequest(event.target.value)}
+              type="text"
+              value={requestItem}
+              onChange={(event) => setRequestItem(event.target.value)}
             />
-            <label> <h3> Description of Item : </h3>  </label>
+            <label>
+              <h3>Description of Item:</h3>
+            </label>
             <input
               placeholder="Type the description."
-              type = "text"
-              value={ request.requestDescription }
-              onChange={(event) => setRequest(event.target.value)}
+              type="text"
+              value={requestDescription}
+              onChange={(event) => setRequestDescription(event.target.value)}
             />
-              <label> <h3> Location for Delivery : </h3>  </label>
+            <label>
+              <h3>Location for Delivery:</h3>
+            </label>
             <input
               placeholder="Type your city and state."
-              type = "text"
-              value={ request.location }
-              onChange={(event) => setRequest(event.target.value)}
+              type="text"
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
             />
           </div>
 
           <div>
-            <button onClick={handleFormSubmit} className="btn btn-info btn-block py-3" type="submit">
+            <button className="btn btn-info btn-block py-3" type="submit">
               Submit your request
             </button>
           </div>
@@ -79,7 +213,15 @@ const RequestForm = ({ requestItem, requestDescription, location  }) => {
           <Link to="/signup">login.</Link>
         </p>
       )}
+
+      {response && (
+        <div>
+          <h2>Response:</h2>
+          <p>{JSON.stringify(response)}</p>
+        </div>
+      )}
     </div>
   );
-      }; 
+};
+
 export default RequestForm;
--- client/src/pages/SingleRequest.js
+++ client/src/pages/SingleRequest.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import '../../src/App.css';
+// import React from 'react';
+// import '../../src/App.css';
 
-return (
-    <div>
-        <p>LOL</p>
-        <h1 className='request'> What are you looking for? </h1>
-    </div>
-);
+// return (
+//     <div>
+//         <p>LOL</p>
+//         <h1 className='request'> What are you looking for? </h1>
+//     </div>
+// );
 
 
 
@@ -54,3 +54,62 @@ return (
 
 
 // export default SingleRequest;
+
+import React from 'react';
+
+// Import the `useParams()` hook
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm';
+
+import { QUERY_SINGLE_REQUEST, QUERY_SINGLE_REQUEST } from '../utils/queries';
+
+const SingleRequest = () => {
+  // Use `useParams()` to retrieve value of the route parameter `:profileId`
+  const { requestId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_REQUEST, {
+    // pass URL parameter
+    variables: { requestId: requestId },
+  });
+
+  const request= data?.request || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="my-3">
+      <h3 className="card-header bg-dark text-light p-2 m-0">
+        {request.requestAuthor} <br />
+        <span style={{ fontSize: '1rem' }}>
+          had this request on {request.createdAt}
+        </span>
+      </h3>
+      <div className="bg-light py-4">
+        <blockquote
+          className="p-4"
+          style={{
+            fontSize: '1.5rem',
+            fontStyle: 'italic',
+            border: '2px dotted #1a1a1a',
+            lineHeight: '1.5',
+          }}
+        >
+          {request.requestText}
+        </blockquote>
+      </div>
+
+      <div className="my-5">
+        <CommentList comments={request.comments} />
+      </div>
+      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+        <CommentForm requestId={request._id} />
+      </div>
+    </div>
+  );
+};
+
+export default SingleRequest;
