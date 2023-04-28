6efec0d2914d240e0897dfc824e223d38d983eda
jordan <jlddukes@gmail.com>
2023-04-27 18:18:56 -0700

    Changed files around that needed new placement

--- client/src/pages/SingleRequest.js
+++ client/src/pages/SingleRequest.js
@@ -8,15 +8,30 @@ return (
     </div>
 );
 
-export default function ToDo() {
-  return (
-    <div className='hero-container'> 
-    <h1 className='products'>To do</h1>
-    <img src='/back/back4.jpeg'/>
-  </div>) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function ToDo() {
+//   return (
+//     <div className='hero-container'> 
+//     <h1 className='products'>To do</h1>
+//     <img src='/back/back4.jpeg'/>
+//   </div>) ;
 
   
-}
+// }
 
 
 
